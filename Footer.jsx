82fdcf63fedf1ff51df68a1637819954a6c6cd27
AsimShakour <shakourasim@gmail.com>
2019-04-06 22:27:13 -0700

    update footer
    styling change req.

--- Footer.jsx
+++ Footer.jsx
@@ -8,17 +8,27 @@ function Footer() {
         <div className="col-xl-12">
           <div className={Styles.footer}>
             <div className={Styles.darkText}>
-              {" "}
-              <h1> Sellers Place </h1>
-              <p style={{ fontSize: "120%" }}>
-                <address>
-                  <a style={{ color: "#e9227b" }} href="https://www.google.com/maps/place/400+Corporate+Pointe+%23300,+Culver+City,+CA+90230"> 400 Corporate Pointe #300 <p> Culver City, CA 90230 </p> </a></address>
+              <div className={Styles.logo}>
+                <img
+                  src="https://sabio-s3.s3.us-west-2.amazonaws.com/sellersplace/0640bbbe-9e54-4b2b-8f67-33a2f662745f_Sellers_Place_Logo_200x91.png"
+                  alt="Seller's Place"
+                />
+              </div>
+              <p style={{ fontSize: "195%" }}>
+                <a style={{ color: "deeppink" }} href="/contactus">
+                  {" "}
+                  <i className="far fa-envelope"></i> {" "} {" "}Contact Us
+                </a>
               </p>
-              <p style={{ fontSize: "120%" }}>
-                <a style={{ color: "#e9227b" }} href="tel:+1-562-307-7589"> (562) 307-7589 </a>
-              </p>
+              <a
+                style={{ color: "deeppink" }}
+                href="https://www.google.com/maps/place/400+Corporate+Pointe+%23300,+Culver+City,+CA+90230"
+              >
+                {" "}
+                400 Corporate Pointe #300<br />Culver City, CA 90230
+                  </a><br />
+              <a style={{ color: "deeppink" }} href="tel:+1-562-307-7589">{" "}(562) 307-7589{" "}</a>
             </div>
-
             <div className="fa-2x mr-2 fab fa-instagram" />
             <div className="fa-2x mr-2 fab fa-twitter" />
             <div className="fa-2x mr-2 fab fa-facebook-f" />
